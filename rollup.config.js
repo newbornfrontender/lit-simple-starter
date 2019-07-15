1c2a6508d84bc9cd6e102c7b69845172c9b3ddc4
Infant Frontender <newbornfrontender@gmail.com>
2019-07-15 09:16:01 +0300

    feat: add rollup-plugin-index-html

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,30 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import indexHTML from 'rollup-plugin-index-html';
 
-import postcssInJs from './plugins/postcss-in-js';
+import postCSSInJS from './plugins/postcss-in-js';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/index.js',
+  // input: 'src/index.js',
+  input: 'src/index.html',
   output: {
     format: 'esm',
     dir: 'public',
     preferConst: true,
+    sourcemap: true,
   },
   plugins: [
     nodeResolve({
       browser: true,
       modulesOnly: true,
     }),
+    indexHTML({
+      minify: production,
+    }),
     babel(),
-    postcssInJs({ production }),
+    postCSSInJS({ production }),
   ],
   treeshake: production,
 };
